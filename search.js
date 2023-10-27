bc7ae0d70e7b917d3ed31a5054b830fa876fc85f
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2023-10-27 12:27:22 -0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,9 @@
+function search(){
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase()
+    let x = document.getElementsByClassName('jogos')
+
+    for(i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase())
+    }
+}
