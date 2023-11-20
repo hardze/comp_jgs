7c10d3eb4e427b34c74599f1dbce31b7eb954033
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2023-11-20 12:17:57 -0300

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-function search(){
-    let input = document.getElementById('searchbar').value
-    input = input.toLowerCase()
-    let x = document.getElementsByClassName('jogos')
-
-    for(i = 0; i < x.length; i++){
-        if(!x[i].innerHTML.toLowerCase())
-    }
-}
